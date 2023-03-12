c98dec32a3950f4874c5dedda170378d5390b450
Phxsha <phashateboho@gmail.com>
2023-03-12 13:20:31 -0700

    uppercase printing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,4 +2,5 @@
 def uppercase(str):
     for c in str:
         if (ord(c) >= 65) and (ord(c) <= 90):
-            print("{}".format(c), end="")
+            c = chr(ord(c))
+        print("{}".format(c), end="")
