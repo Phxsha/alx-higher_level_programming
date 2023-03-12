c5923aacd65d64db3447bb7d61914ebaf9cb8411
Phxsha <phashateboho@gmail.com>
2023-03-12 12:32:20 -0700

    combination of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if (i == 8) and (j == 9):
-            print("{}{}".format(i,j))
-        elif i < j:
+        if i < j:
             print("{}{}".format(i, j), end=", ")
+        if (i == 8) and (j == 9):
+            print("{}{}".format(i, j))
