c9d9ae07f632bfb1046fa6aecff1721f2417e8c8
Phxsha <phashateboho@gmail.com>
2024-01-02 20:27:02 +0200

    Update

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        print(abs(number) % 10, end="")
+        return(abs(number) % 10)
+    else:
+        print(abs(number) % 10, end="")
+        return(abs(number) % 10)
