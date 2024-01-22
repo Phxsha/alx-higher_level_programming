c84ec5f2dcda5f3ffb924e4580030c18fce2a1af
Phxsha <phashateboho@gmail.com>
2024-01-22 06:57:51 -0800

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    printed_integers = 0
+
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                printed_integers += 1
+        print()
+    except IndexError:
+        pass
+
+    return printed_integers
