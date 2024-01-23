d7fa19727097a910855749938418e61a8c305910
Phxsha <phashateboho@gmail.com>
2024-01-23 01:59:01 -0800

    OOP tasks defining a square and its properties

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a class Square """
+class Square:
+    """class that defines a square"""
+    def __init__(self, size=0):
+        """ initialization of size of the square.
+        Args:
+            size (int): The size of new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
