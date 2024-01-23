b58b59e056da40f032cf3a768c9a50e6a5a1ea0d
Phxsha <phashateboho@gmail.com>
2024-01-23 09:35:26 -0800

    fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,10 +4,10 @@
 
 class Square:
         """class that defines a square"""
+
         def __init__(self, size):
             """ initialization of size of the square.
             Args:
                 size (int): The size of new square.
             """
-
             self.__size = size
