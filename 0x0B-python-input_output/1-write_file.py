9fe0911bb90163d6efa5b2b7bde7582f1099c3a9
Phxsha <phashateboho@gmail.com>
2024-02-06 20:19:32 +0200

    write a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a text file writing function"""
+
+
+def read_file(filename="", text=""):
+    """Function that writes to a file.
+
+    Args:
+        filename (any): The file to write on.
+        text (string): text to write to the file.
+    Return:
+        Written file..
+    """
+    with open(filename, mode='w', encoding='UTF-8') as file:
+        return file.write(text)
