50cd4fff7ea7f39e7cc8fe88e8e3ee7a325af0fb
Phxsha <phashateboho@gmail.com>
2024-02-06 21:04:39 +0200

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Defines a python class to JSON function"""
-import json
 
 
 def class_to_json(obj):
     """Returns class to obj"""
-    return json.__dict__
+    return obj.__dict__
