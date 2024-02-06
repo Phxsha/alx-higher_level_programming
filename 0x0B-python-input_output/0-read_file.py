8d9cd1552e4356019c472373c49fbf54fed63a40
Phxsha <phashateboho@gmail.com>
2024-02-06 20:06:00 +0200

    task 0 readfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """Function that reads a file.
+
+    Args:
+        filename (any): The file to read.
+    Return:
+        Nothing.
+    """
+    with open(filename, encoding='UTF-8') as file:
+        print(file.read(), end="")
