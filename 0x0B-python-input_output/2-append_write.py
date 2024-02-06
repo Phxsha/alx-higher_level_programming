05e3c21f1b6fcab3366f80551117a886189bbf52
Phxsha <phashateboho@gmail.com>
2024-02-06 20:28:28 +0200

    appending to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a text file appending function"""
+
+
+def append_file(filename="", text=""):
+    """Function that appends to a file.
+
+    Args:
+        filename (any): The file to write on.
+        text (string): text to append to the file.
+    Return:
+        Written file.
+    """
+    with open(filename, mode='a', encoding='UTF-8') as file:
+        return file.write(text)
