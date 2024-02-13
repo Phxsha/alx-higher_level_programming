ea7530b8c729f84e588d4a5ee56b7cd7fc15c0c2
Phxsha <phashateboho@gmail.com>
2024-02-13 20:56:59 +0200

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a base Class for the id used in the project"""
+
+
+class Bass:
+    """Base Class for managing id attribute"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize base instance"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
