c43f6c37d88dbbc0e67e737064ce18db7b5d51a0
Phxsha <phashateboho@gmail.com>
2024-02-14 13:45:01 +0200

    square and tests

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize a square
+
+        Args:
+            size (int): The size of the new square
+            x (int): x coordinate of the square
+            y (int): y coordinate of the new square
+            id (int): The identity of the new square
+        """
+        super().__init__(size, x, y, id)
+
+        @property
+        def size(self):
+            """Get/set the size of Square"""
+            return self.width
+
+        @size.setter
+        def size(self, value):
+            self.width = value
+            self.height = value
