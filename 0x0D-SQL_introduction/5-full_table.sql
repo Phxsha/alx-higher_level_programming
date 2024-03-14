204dc5d57d64dfc7594dfb10673e7cba42da4acd
Phxsha <phashateboho@gmail.com>
2024-03-14 14:17:56 -0700

    show descriptionof table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Prints full description of a specitfied table in a database
 -- Print full description of first_table
-SELECT TABLE_NAME, CREATE_TABLE
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE 'first_table';
 
