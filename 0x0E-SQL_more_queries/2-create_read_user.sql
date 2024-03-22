167f137e51e09dba88c74e8f672d129013400534
Phxsha <phashateboho@gmail.com>
2024-03-22 02:34:06 -0700

    Creates a database, then creates a new user, then gives user SELECt privileges on the newly created database

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Script to create database hbtn_0d_2 and user user_0d_2
+
+-- Create database hbtn_0d_2 if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user user_0d_2 if not exists and set password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to user_0d_2 on database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
