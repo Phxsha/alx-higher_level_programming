75925569aebb1654593cc458e72f58c72d1e76b7
Phxsha <phashateboho@gmail.com>
2024-03-22 02:23:01 -0700

    Shows permissions for certain users on your database

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Script to list all privileges of MySQL users user_0d_1 and user_0d_2 on the server
+
+-- Show grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+This includes more advanced sql tasks, including new users, granting permissions, and executions, more on JOIN as well
