47cd22b36f69196081f2177186f5e54c4af20eb8
Phxsha <phashateboho@gmail.com>
2024-04-10 09:33:59 -0700

    task 5

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      return {}; // Return empty object if width or height is not positive
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    // Swap width and height
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    // Double the width and height
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
