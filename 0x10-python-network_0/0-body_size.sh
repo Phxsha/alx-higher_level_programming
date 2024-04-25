05a5263ec26f33e48c101900a1ea1e33415c97f6
root <phashateboho@gmail.com>
2024-04-25 07:29:38 -0700

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # This script takes a URL, sends a request, and displays the size of the response body
 
-# Send a GET request to the URL and store the response body in a temporary file
-curl -s -o temp_body.txt "$1"
-
-# Get the size of the response body in bytes and display it
-cat temp_body.txt | wc -c
-
-# Clean up temporary file
-rm temp_body.txt
+# Send a GET request to the URL, discard header, and get the size of the body
+curl -s -o /dev/null -w '%{size_download}' "$1"
