93cc6a2aa470c6332efde8e0a586798f1f5fc6f7
root <phashateboho@gmail.com>
2024-06-02 11:59:27 -0700

    semistandard check

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-    $('#btn_translate').click(function() {
-        const languageCode = $('#language_code').val();
-        $.ajax({
-            url: `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`,
-            success: function(data) {
-                $('#hello').text(data.hello);
-            },
-            error: function() {
-                $('#hello').text('Translation not found');
-            }
-        });
+$(document).ready(function () {
+  $('#btn_translate').click(function () {
+    const languageCode = $('#language_code').val();
+    $.ajax({
+      url: `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`,
+      success: function (data) {
+        $('#hello').text(data.hello);
+      },
+      error: function () {
+        $('#hello').text('Translation not found');
+      }
     });
+  });
 
-    $('#language_code').keypress(function(event) {
-        if (event.which == 13) {
-            $('#btn_translate').click();
-        }
-    });
+  $('#language_code').keypress(function (event) {
+    if (event.which === 13) {
+      $('#btn_translate').click();
+    }
+  });
 });
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
-        const movies = data.results;
-        for (const movie of movies) {
-            $('#list_movies').append('<li>' + movie.title + '</li>');
-        }
-    });
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    const movies = data.results;
+    for (const movie of movies) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    }
+  });
 });
